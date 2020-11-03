d6deb24cb634f60714b3b84df71f5955a8347cf8
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2020-11-03 11:09:55 +0800

    캐릭터움직임
    캐릭터 움직이는 버튼에 대한 스크립트, 버튼 이미지, 캐릭터 이미지, 캐릭터 움직임에 관한 스크립트

---
+++ Assets/Scripts/GameBtnManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBtnManager : MonoBehaviour
+{
+    GameObject player;
+    PlayerMovement playerMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerMovement = player.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void LeftBtnDown()
+    {
+        playerMovement.LeftMove = true;
+    }
+    public void LeftBtnUp()
+    {
+        playerMovement.LeftMove = false;
+    }
+    public void RightBtnDown()
+    {
+        playerMovement.RightMove = true;
+    }
+    public void RighBtntUp()
+    {
+        playerMovement.RightMove = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movePower = 1f;
+    public bool LeftMove = false;
+    public bool RightMove = false;
+    Rigidbody2D rigid;
+    Vector3 movement;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        move();
+    }
+    void move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+        if(LeftMove ==true)
+        {
+            moveVelocity = Vector3.left;
+            animator.SetBool("IsWalk", true);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (RightMove==true)
+        {
+            moveVelocity = Vector3.right;
+            animator.SetBool("IsWalk", true);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            animator.SetBool("IsWalk", false);
+        }
+            transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+}
